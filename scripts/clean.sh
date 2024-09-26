d715dccb846260bb7c926ff1a4ca7b4c94294476
Christoph Pader <chris@margelo.com>
2024-09-26 20:53:47 +0200

    update scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,4 +1,4 @@
 export JRE_DIR="./jre"
-export LIB_DIR="$dir/lib"
+export LIB_DIR="./lib"
 
-rm -rf $JRE_DIR $LIB_DIR
+rm -rf $JRE_DIR $LIB_DIR/*.jar
--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -3,7 +3,7 @@ export JRE_DIR="$dir/jre"
 export LIB_DIR="$dir/lib"
 export FX_DIR="$LIB_DIR/javafx-jmods-22.0.2"
 
-./clean.sh
+./scripts/clean.sh
 
 jlink --module-path $JAVA_HOME/jmods:$FX_DIR --add-modules java.base,java.logging,javafx.base,javafx.swing,javafx.controls,javafx.graphics,javafx.fxml --output $JRE_DIR
 
