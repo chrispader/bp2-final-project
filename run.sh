6158a25cabbafa4a8d01c525c1560f7e5822a00c
Christoph Pader <chris@margelo.com>
2024-09-26 20:30:49 +0200

    update run script

--- run.sh
+++ run.sh
@@ -2,12 +2,7 @@ dir=$(echo "$PWD")
 export JRE_DIR="$dir/jre"
 export LIB_DIR="$dir/lib"
 
-export HANSOLO_LIBS="$LIB_DIR/hansolo"
-export HANSOLO_CHARTS="$HANSOLO_LIBS/charts.jar"
-export HANSOLO_TOOLBOX="$HANSOLO_LIBS/toolbox.jar"
-export HANSOLO_TOOLBOXFX="$HANSOLO_LIBS/toolboxfx.jar"
-
-export BSF4Rexx_JavaStartupOptions="-cp $CLASSPATH:$HANSOLO_TOOLBOX:$HANSOLO_TOOLBOXFX:$HANSOLO_CHARTS"
+export BSF4Rexx_JavaStartupOptions="-cp $CLASSPATH:$LIB_DIR/*"
 
 export JAVA_HOME=$JRE_DIR
 export PATH=$JRE_DIR/bin:$PATH
