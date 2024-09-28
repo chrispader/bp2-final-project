792e11dc17c3ad58de161b32740afc242d068d4d
Christoph Pader <chris@margelo.com>
2024-09-28 14:40:59 +0200

    update build scritp

--- scripts/build-fx-jre.ps1
+++ scripts/build-fx-jre.ps1
@@ -1,17 +1,18 @@
 # Get the directory of the current script
 $SCRIPT_DIR = Split-Path -Parent $MyInvocation.MyCommand.Definition
 
-# Change to the script directory
-Set-Location -Path $SCRIPT_DIR
-
 . "$SCRIPT_DIR\env.ps1"
+
+# Check if the JavaFX directory exists
+if (-Not (Test-Path -Path $env:JAVAFX_DIR)) {
+  Write-Error "JavaFX directory '$env:JAVAFX_DIR' does not exist. Build cannot proceed."
+  exit 1  # Exit with a non-zero status to indicate an error
+}
 
 . "$SCRIPT_DIR\clean.ps1"
 
-Write-Host env:JAVAFX_DIR
-
 # Build custom JRE with JavaFX modules
-jlink --module-path "$env:JAVA_HOME\jmods;$env:JAVAFX_DIR" --add-modules java.base,java.logging,javafx.base,javafx.swing,javafx.controls,javafx.graphics,javafx.fxml --output $env:JRE_DIR
+jlink --module-path "$env:JAVA_HOME\jmods;$env:JAVAFX_DIR" --add-modules "java.base,java.logging,javafx.base,javafx.swing,javafx.controls,javafx.graphics,javafx.fxml" --output "$env:JRE_DIR"
 
 # Call Gradle to download dependencies
-Start-Process -FilePath "..\gradlew.bat" -ArgumentList "download" -NoNewWindow
+Start-Process -FilePath ".\gradlew.bat" -ArgumentList "download" -NoNewWindow
